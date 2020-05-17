c245670c74c87f8675fb61ecbab28f61edbd13d3
Maksim Vlasov <vlasov.maksim@hotmail.com>
2020-05-17 20:39:07 +0300

    Add server hello world app

---
+++ demo-server/src/test/kotlin/com/maks/demoserver/DemoServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.maks.demoserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
